0331bc02f00e153c6e559bfeb533c12bc5315bc3
Mads <mads.heilberg@gmail.com>
2018-02-27 12:32:01 +0100

    Add files via upload

---
+++ Chapter20-nodejs/search_tool.js
@@ -0,0 +1,26 @@
+console.log("[SEARCH TOOL INITIATED]");
+//import of File System methods
+const {statSync, readdirSync, readFileSync} = require("fs");
+
+//creates searchString
+var searchString = new RegExp(process.argv[2]);
+
+//runs search method on path
+for (var path of process.argv.slice(3)){
+    console.log("Path: " + path);
+    search(path);
+}
+
+//checks for isDirectory (then recursively searches on pattern in directory files)
+//if pattern match file content - log file name
+function search(file){
+    console.log("[SEARCHING FILES..]");
+    var fileStat = statSync(file);
+    if(fileStat.isDirectory()){
+        for(var name of readdirSync(file)){
+            search(file + "/" + name);
+        }
+    } else if (searchString.test(readFileSync(file))){
+        console.log("File found: " + file);
+    }
+}
